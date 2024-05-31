6df5dd81b85139544d1006103a19d9bc03783840
Luzayadio Vila <71708339+luzayadio@users.noreply.github.com>
2024-05-31 13:17:32 +0100

    first layout (put the buttons)

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -4,12 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.calculator.ui.theme.CalculatorTheme
 
@@ -20,9 +27,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             CalculatorTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    CalculatorApp(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
                     )
                 }
             }
@@ -31,17 +39,140 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CalculatorApp(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth().weight(1F),
+            horizontalArrangement = Arrangement.End
+        ) {
+            Text(
+                "0",
+            )
+        }
+        Row(
+            modifier = Modifier.weight(1F),
+        ) {
+            Button(
+                modifier = Modifier.weight(1F).fillMaxHeight(),
+                onClick = {  }) {
+                Text("C")
+            }
+            Button(
+                modifier = Modifier.weight(1F).fillMaxHeight(),
+                onClick = {  }) {
+                Text("+/-")
+            }
+            Button(
+                modifier = Modifier.weight(1F).fillMaxHeight(),
+                onClick = {  }) {
+                Text("%")
+            }
+            Button(
+                modifier = Modifier.weight(1F).fillMaxHeight(),
+                onClick = {  }) {
+                Text("/")
+            }
+        }
+        Row(
+            modifier = Modifier.weight(1F),
+        ) {
+            Button(
+                modifier = Modifier.weight(1F).fillMaxHeight(),
+                onClick = {  }) {
+                Text("7")
+            }
+            Button(
+                modifier = Modifier.weight(1F).fillMaxHeight(),
+                onClick = {  }) {
+                Text("8")
+            }
+            Button(
+                modifier = Modifier.weight(1F).fillMaxHeight(),
+                onClick = {  }) {
+                Text("9")
+            }
+            Button(
+                modifier = Modifier.weight(1F).fillMaxHeight(),
+                onClick = {  }) {
+                Text("X")
+            }
+        }
+        Row(
+            modifier = Modifier.weight(1F),
+        ) {
+            Button(
+                modifier = Modifier.weight(1F).fillMaxHeight(),
+                onClick = {  }) {
+                Text("4")
+            }
+            Button(
+                modifier = Modifier.weight(1F).fillMaxHeight(),
+                onClick = {  }) {
+                Text("5")
+            }
+            Button(
+                modifier = Modifier.weight(1F).fillMaxHeight(),
+                onClick = {  }) {
+                Text("6")
+            }
+            Button(
+                modifier = Modifier.weight(1F).fillMaxHeight(),
+                onClick = {  }) {
+                Text("-")
+            }
+        }
+        Row(
+            modifier = Modifier.weight(1F),
+        ) {
+            Button(
+                modifier = Modifier.weight(1F).fillMaxHeight(),
+                onClick = {  }) {
+                Text("1")
+            }
+            Button(
+                modifier = Modifier.weight(1F).fillMaxHeight(),
+                onClick = {  }) {
+                Text("2")
+            }
+            Button(
+                modifier = Modifier.weight(1F).fillMaxHeight(),
+                onClick = {  }) {
+                Text("3")
+            }
+            Button(
+                modifier = Modifier.weight(1F).fillMaxHeight(),
+                onClick = {  }) {
+                Text("+")
+            }
+        }
+        Row(
+            modifier = Modifier.weight(1F),
+        ) {
+            Button(
+                modifier = Modifier.weight(2F).fillMaxHeight(),
+                onClick = {  }) {
+                Text("0")
+            }
+            Button(
+                modifier = Modifier.weight(1F).fillMaxHeight(),
+                onClick = {  }) {
+                Text(",")
+            }
+            Button(
+                modifier = Modifier.weight(1F).fillMaxHeight(),
+                onClick = {  }) {
+                Text("=")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CalculatorPreview() {
     CalculatorTheme {
-        Greeting("Android")
+        CalculatorApp(modifier = Modifier.fillMaxSize())
     }
 }
